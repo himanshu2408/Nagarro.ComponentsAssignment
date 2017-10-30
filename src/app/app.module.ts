80d5522b6be953369a808be3c9125f02656ce272
Himanshu Singh <himanshu.singh@nagarro.com>
2017-10-31 00:05:05 +0530

    github pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './login/login.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
